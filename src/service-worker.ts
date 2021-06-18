3772177b640680e1317b02f3f8d8184d41db9cc4
Glenn Hope <glenn.alexander.hope@gmail.com>
2021-06-18 15:42:45 -0700

    Make service worker cache CSV files as well

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -8,11 +8,11 @@
 // You can also remove this file if you'd prefer not to use a
 // service worker, and the Workbox build step will be skipped.
 
-import { clientsClaim } from 'workbox-core';
-import { ExpirationPlugin } from 'workbox-expiration';
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import {clientsClaim} from 'workbox-core';
+import {ExpirationPlugin} from 'workbox-expiration';
+import {precacheAndRoute, createHandlerBoundToURL} from 'workbox-precaching';
+import {registerRoute} from 'workbox-routing';
+import {StaleWhileRevalidate, CacheFirst} from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -29,65 +29,73 @@ precacheAndRoute(self.__WB_MANIFEST);
 // https://developers.google.com/web/fundamentals/architecture/app-shell
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
-  ({ request, url }: { request: Request; url: URL }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') {
-      return false;
-    }
+    // Return false to exempt requests from being fulfilled by index.html.
+    ({request, url}: {request: Request; url: URL}) => {
+        // If this isn't a navigation, skip.
+        if (request.mode !== 'navigate') {
+            return false;
+        }
 
-    // If this is a URL that starts with /_, skip.
-    if (url.pathname.startsWith('/_')) {
-      return false;
-    }
+        // If this is a URL that starts with /_, skip.
+        if (url.pathname.startsWith('/_')) {
+            return false;
+        }
 
-    // If this looks like a URL for a resource, because it contains
-    // a file extension, skip.
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    }
+        // If this looks like a URL for a resource, because it contains
+        // a file extension, skip.
+        if (url.pathname.match(fileExtensionRegexp)) {
+            return false;
+        }
 
-    // Return true to signal that we want to use the handler.
-    return true;
-  },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+        // Return true to signal that we want to use the handler.
+        return true;
+    },
+    createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
+    // Add in any other file extensions or routing criteria as needed.
     // TODO: check against google fonts? any other checks?
     // url.origin === self.location.origin &&
-  ({ url }) => url.pathname.endsWith('.woff2'),
-  // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: 'fonts',
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 150 }),
-    ],
-  })
+    ({url}) => url.pathname.endsWith('.woff2'),
+    // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    new StaleWhileRevalidate({
+        cacheName: 'fonts',
+        plugins: [
+            // Ensure that once this runtime cache reaches a maximum size the
+            // least-recently used images are removed.
+            new ExpirationPlugin({maxEntries: 150}),
+        ],
+    })
 );
+
+
+function fileMatcher(opts: {url: URL}): boolean {
+    const {url} = opts;
+    const locationCorrect = url.pathname.startsWith('/db/');
+    const matchFileType = !!url.pathname.match(/(\.json|\.csv)$/);
+    return locationCorrect && matchFileType;
+}
 
 // TODO: version databases so newly-generated DBs will overwrite old ones
 registerRoute(
-  ({ url }) => url.pathname.startsWith('/db/') && url.pathname.endsWith('.json'),
-  new CacheFirst({
-    cacheName: 'databases',
-    plugins: [
-      new ExpirationPlugin({ purgeOnQuotaError: false }),
-    ],
-  })
+    fileMatcher,
+    new CacheFirst({
+        cacheName: 'databases',
+        plugins: [
+            new ExpirationPlugin({purgeOnQuotaError: false}),
+        ],
+    })
 );
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
 
 // Any other custom service worker logic can go here.
