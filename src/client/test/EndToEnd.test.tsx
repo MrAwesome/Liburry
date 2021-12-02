dc774d25318dfd44b92d30e011ff54c5a9f7b802
Glenn Hope <glenn.alexander.hope@gmail.com>
2021-12-02 12:36:53 -0800

    Real end-to-end test using playwright. taigi.us specific for now.

---
+++ src/client/test/EndToEnd.test.tsx
@@ -0,0 +1,33 @@
+import { chromium, Browser, Page, Response as Resp } from "playwright";
+
+// NOTE: each test here starts a new browser, so add new ones sparingly
+describe("Main End-To-End Tests", () => {
+    let response: Resp | null;
+    let browser: Browser;
+    let page: Page;
+
+    beforeAll(async () => {
+        browser = await chromium.launch({args: ["--ignore-certificate-errors"]});
+        page = await browser.newPage();
+        //response = await page.goto("https://localhost:3000/");
+        response = await page.goto("https://localhost:3000/#m=SEARCH;q=alexander");
+    });
+
+    afterAll(async () => {
+        await browser?.close();
+    });
+
+    test("searchbar loads", async () => {
+        // Get the HTTP status code of the response. A 200 means it loaded successfully!
+        expect(response?.status()).toBe(200);
+        const inputElem = await page?.waitForSelector("input");
+        const placeholderObj = await inputElem.getProperty("placeholder");
+        expect(await placeholderObj.jsonValue()).toBe("Search...");
+    });
+
+    test("taigi.us specific POJ search", async () => {
+        const vocabElem = await page?.waitForSelector(".vocab-element");
+        const myname = vocabElem.asElement();
+        expect(await myname.innerText()).toBe("A-le̍k-san-tāi");
+    });
+});
